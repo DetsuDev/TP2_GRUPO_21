d3b3e8c1ec6985f5f1f57609369892a69bf88756
Sebastian Fuentes <sebastianfuentesjhi@gmail.com>
2026-04-17 12:18:20 -0300

    fix

---
+++ TP2_GRUPO_21/Ejercicio2a.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace TP2_GRUPO_21
+{
+    public partial class Ejercicio2a : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnVerResumen_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
