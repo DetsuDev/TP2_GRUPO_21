d29fb63820e96a0d2e8e3d5f030093693b6cc561
Santiago <santirom1905@gmail.com>
2026-04-20 16:45:21 -0300

    se agregan cambios punto 1 y 4

--- TP2_GRUPO_21/Ejercicio1.aspx.cs
+++ TP2_GRUPO_21/Ejercicio1.aspx.cs
@@ -23,10 +23,11 @@ namespace TP2_GRUPO_21
             int total = cantidad1 + cantidad2;
 
             string tabla = "<table border='1' style='background-color:#fff4ed'>";
-            tabla += "<tr><th>Producto</th><th>Cantidad</th><th>Valor total</th></tr>";
-            tabla += "<tr><td>" + producto1 + "</td><td>" + cantidad1 + "</td><td> $" + valor_total1 + "</td></tr>";
-            tabla += "<tr><td>" + producto2 + "</td><td>" + cantidad2 + "</td><td> $" + valor_total2 + "</td></tr>";
-            tabla += "<tr style='background-color:#ffe8d4' !important><td> Total </td><td>" + total + "</td><td> $" + valor_final + "</td></tr>";
+            tabla += "<tr><th>Producto</th><th>Cantidad</th></tr>";
+            tabla += "<tr><td>" + producto1 + "</td><td>" + cantidad1 + "</td></tr>";
+            tabla += "<tr><td>" + producto2 + "</td><td>" + cantidad2 + "</td></tr>";
+            tabla += "<tr style='background-color:#ffe8d4'><td>Total</td><td>" + total + "</td></tr>";
+            tabla += "</table>";
             lblTabla.Text = tabla;
         }
     }
--- TP2_GRUPO_21/Ejercicio4.aspx
+++ TP2_GRUPO_21/Ejercicio4.aspx
@@ -1,14 +1,17 @@
 ﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Ejercicio4.aspx.cs" Inherits="TP2_GRUPO_21.Ejercicio4" %>
-
 <!DOCTYPE html>
-
 <html xmlns="http://www.w3.org/1999/xhtml">
-<head runat="server">
-    <title></title>
-</head>
-<body> 
+<head runat="server"><title>Ejercicio 4</title></head>
+<body>
     <form id="form1" runat="server">
         <div>
+            <asp:Label ID="lblUsuario" runat="server" Text="Usuario:"></asp:Label>
+            <asp:TextBox ID="txtUsuario" runat="server"></asp:TextBox>
+            <br />
+            <asp:Label ID="lblClave" runat="server" Text="Clave:"></asp:Label>
+            <asp:TextBox ID="txtClave" runat="server" TextMode="Password"></asp:TextBox>
+            <br />
+            <asp:Button ID="btnValidar" runat="server" Text="Validar" OnClick="btnValidar_Click" />
         </div>
     </form>
 </body>
